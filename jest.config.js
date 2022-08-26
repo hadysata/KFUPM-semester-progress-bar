8e8c47447f9ce86ae6d5b2e56f1e0f8d6e0c25aa
Hady <hadysata@gmail.com>
2022-08-27 01:29:48 +0300

    chore: clean template

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,6 @@ export default {
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.(m)?js$': '$1',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(m)?ts$',
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.ts',
