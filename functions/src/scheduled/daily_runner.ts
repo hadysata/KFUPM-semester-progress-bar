0d64449b3de2d8e3b5fc1da5b036f9d75ebd20d1
Hady <hadysata@gmail.com>
2022-08-29 11:46:51 +0300

    chore: change `dailyRunner` time from 12 PM to 10 AM

--- functions/src/scheduled/daily_runner.ts
+++ functions/src/scheduled/daily_runner.ts
@@ -2,7 +2,7 @@ import * as functions from 'firebase-functions';
 import * as BotRunner from '../../../src/runner';
 
 const dailyRunner = functions.pubsub
-  .schedule('0 12 * * *') // Run every day, 12:00 PM
+  .schedule('0 10 * * *') // Run every day, 10:00 AM
   .timeZone('Asia/Riyadh')
   .onRun(async (_) => {
     BotRunner.runner();
