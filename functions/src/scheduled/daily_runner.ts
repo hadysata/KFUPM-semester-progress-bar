4f772e82e2e5594e42344df36079cfc3a53c1ece
Hady <hadysata@gmail.com>
2022-11-14 12:50:20 +0300

    chore: remove unused vars

--- functions/src/scheduled/daily_runner.ts
+++ functions/src/scheduled/daily_runner.ts
@@ -4,7 +4,7 @@ import * as BotRunner from '../../../src/runner';
 const dailyRunner = functions.pubsub
   .schedule('0,10,30 10,11,15 * * *') // Run every day, At minute 0, 10, and 30 past hour 10, 11, 12, and 15
   .timeZone('Asia/Riyadh')
-  .onRun(async (_) => {
+  .onRun(async () => {
     BotRunner.runner();
   });
 
